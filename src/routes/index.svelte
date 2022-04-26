cf37d59e411bb66a985bfaebef81ebb08fc007b0
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-04-26 19:44:31 +0200

    forward to docs

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,14 @@
 <script>
-    import A from './docs/[...index].svelte'
+    import { goto } from '@roxi/routify'
+    setTimeout(() => {
+        $goto('/docs')
+    }, 5000)
 </script>
 
 <div class="container">
     <img class="brand" src="/assets/routify3.svg" width="600" />
     <h1>Work in Progress</h1>
-    <a href="/docs">DOCS</a>
+    Forwarding to<a href="/docs">DOCS</a>...
 </div>
 
 <style>
