e8a269ad8b246e93489ce35784013837b99e61bf
Jakob Rosenberg <jakobrosenberg@gmail.com>
2022-04-27 20:29:15 +0200

    chore: missing space

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,7 @@
 <div class="container">
     <img class="brand" src="/assets/routify3.svg" width="600" />
     <h1>Work in Progress</h1>
-    Forwarding to<a href="/docs">DOCS</a>...
+    Forwarding to <a href="/docs">DOCS</a>...
 </div>
 
 <style>
