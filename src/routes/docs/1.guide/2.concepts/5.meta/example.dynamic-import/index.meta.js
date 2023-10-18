fea94d2e6ea3b32ca90ae47e69f752abe7b54b0b
Jakob Rosenberg <jakobrosenberg@gmail.com>
2023-10-18 13:16:03 +0200

    fix: swapi

--- src/routes/docs/1.guide/2.concepts/5.meta/example.dynamic-import/index.meta.js
+++ src/routes/docs/1.guide/2.concepts/5.meta/example.dynamic-import/index.meta.js
@@ -8,7 +8,7 @@
  */
 export default async ({ split }) => {
     // fetch some movies
-    const response = await fetch('https://swapi.dev/api/films');
+    const response = await fetch('https://swapi.dev/api/films/?format=json');
     const data = await response.json();
     const films = data.results
 
