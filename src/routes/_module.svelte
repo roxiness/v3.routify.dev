a7c0f3de63ca16fd7dd28ad8ce9ba99e0c5b1b57
Jakob Rosenberg <jakobrosenberg@gmail.com>
2023-12-05 23:59:08 +0100

    update navigation handling

--- src/routes/_module.svelte
+++ src/routes/_module.svelte
@@ -1,14 +1,167 @@
 <script>
-    import { activeRoute } from '@roxi/routify'
+    import Debugger from '@roxi/routify/lib/runtime/decorators/Debugger.svelte'
+    import { isActive, afterUrlChange } from '@roxi/routify'
     import TopNav from './__layout/TopNav.svelte'
-    import { Window } from 'polykit'
+    import { Window, Sidenav, Backdrop } from 'polykit'
+    import Nav from './docs/_sidenav.svelte'
+    import Links from './__layout/Links.svelte'
+
+    export let context
+
+    let open
+    let state
+    let screenSize
+    let update
+    let isLoading = ''
+
+    export let presets = {
+        default: { open: true, transform: 'shrink' },
+        mobile: { open: false, transform: '' },
+        tablet: { open: false, transform: 'slide' },
+        desktop: { open: $isActive('./docs'), transform: 'shrink' },
+    }
+
+    $afterUrlChange(ctx => {
+        isLoading = 'is-loading'
+        setTimeout(() => (isLoading = ''), 100)
+        open = $isActive('./docs') && $screenSize === 'desktop'
+    })
 </script>
 
-<Window let:size>
-    <div class="app {size}">
-        {#if $activeRoute.url !== '/' && size !== 'mobile'}
+<Window let:size bind:screenSize>
+    <div class="app {state} {isLoading}">
+        {#if size === 'desktop'}
             <TopNav />
         {/if}
-        <slot />
+        <Sidenav bind:open bind:state asidePositioning="absolute" {presets} bind:update>
+            <aside slot="aside" let:toggle>
+                <div class="sidenav-activator" on:click={toggle}>☰</div>
+                <div class="container nav">
+                    <a href="/">
+                        <img alt="Routify" class="brand" src="/assets/routify3.svg" />
+                    </a>
+                    <div class="links">
+                        <Links />
+                        <hr />
+                    </div>
+                    {#if $isActive('./docs')}
+                        <Nav node={context.node.traverse('./docs')} />
+                    {/if}
+                </div>
+            </aside>
+            {#if ['mobile', 'tablet'].includes(size)}
+                <Backdrop bind:show={open} />
+            {/if}
+
+            <div style="width: 100%">
+                <slot />
+            </div>
+        </Sidenav>
     </div>
 </Window>
+
+<style>
+    :global(.is-loading > .polykit-sidenav > *) {
+        transition: none !important;
+    }
+
+    .brand {
+        display: block;
+        width: 160px;
+        margin-top: -56px;
+        margin-bottom: 32px;
+        top: 16px;
+    }
+
+    /**
+    * Global
+    */
+    /* :global(.container) {
+        padding: 0 var(--spacing-4);
+    } */
+
+    .container.outer {
+        padding: 0;
+    }
+
+    .container.nav {
+        overflow-y: auto;
+        padding-top: 72px;
+        height: 100%;
+    }
+
+    .mobile .container.nav,
+    .tablet .container.nav {
+        padding-left: 24px;
+        padding-right: 24px;
+    }
+
+    main {
+        min-height: 400px;
+        padding-top: 72px;
+    }
+    aside {
+        position: fixed;
+        width: inherit;
+        background: #fdf6fe;
+        height: 100vh;
+    }
+
+    aside,
+    .sidenav-activator {
+        transition: all 0.4s;
+    }
+
+    .sidenav-activator {
+        position: absolute;
+        z-index: 1;
+        padding: 4px 8px;
+        right: 20px;
+        top: 16px;
+        font-weight: bolder;
+        background: white;
+        border-radius: 4px;
+        cursor: pointer;
+        border: 2px #ffa8fa solid;
+    }
+
+    :global(.closed) .sidenav-activator {
+        right: -48px;
+    }
+
+    /**
+    * Mobile
+    */
+    .mobile main {
+        padding: var(--spacing-6) var(--spacing-4);
+    }
+
+    .mobile aside,
+    .tablet aside {
+        top: 0;
+        height: 100%;
+        z-index: 1;
+        background: white;
+    }
+
+    .mobile.open aside {
+        box-shadow: 0 0 18px -3px rgba(0 0 0 / 0.4);
+    }
+
+    /**
+    * Mobile
+    */
+    .mobile main {
+        margin: 0;
+        border-radius: 0;
+    }
+
+    /**
+    * Desktop
+    */
+    :global(.desktop) .brand,
+    :global(.desktop) .links,
+    :global(.desktop) .sidenav-activator {
+        display: none;
+    }
+</style>
--- src/routes/docs/_module.svelte
+++ src/routes/docs/_module.svelte
@@ -7,162 +7,44 @@
     export let context
     import Disclaimer from './_disclaimer.svelte'
 
-    let decorator = import.meta.env.DEV ? [Debugger] : []
+    // const decorator = import.meta.env.DEV ? [Debugger] : []
+    const decorator = null
 
-    let open
-    let state
+    const anchor = elem =>
+        elem.previousElementSibling || elem.parentElement.previousElementSibling
 </script>
 
 <!-- routify:meta bundle -->
 
-<div class="app {$screenSize} {state}">
-    <div class="container outer">
-        <Sidenav bind:open bind:state asidePositioning="absolute">
-            <aside slot="aside" let:toggle>
-                <div class="sidenav-activator" on:click={toggle}>☰</div>
-                <div class="container nav">
-                    <img alt="Routify" class="brand" src="/assets/routify3.svg" />
-                    <div class="links">
-                        <Links />
-                        <hr />
-                    </div>
-                    <Nav node={context.node} />
-                </div>
-            </aside>
-            {#if ['mobile', 'tablet'].includes($screenSize)}
-                <Backdrop bind:show={open} />
-            {/if}
-
-            <div class="container main">
-                <main class="copy">
-                    <Disclaimer />
-                    <slot inline {decorator} anchor="wrapper"/>
-                </main>
-            </div>
-        </Sidenav>
-    </div>
+<div class="container main">
+    <main class="copy">
+        <Disclaimer />
+        <slot inline {decorator} {anchor} />
+    </main>
 </div>
 
 <style>
-    .tablet .copy {
+    :global(.tablet) .copy {
         margin: 0 var(--spacing-7);
     }
 
-    .desktop .copy {
+    :global(.desktop) .copy {
         margin: 0 var(--spacing-9);
     }
 
     .container.main {
         padding: 0;
     }
-    .brand {
-        display: none;
-    }
-    .mobile .brand {
-        display: block;
-        width: 160px;
-        margin-top: -56px;
-        margin-bottom: 32px;
-        /* position: absolute; */
-        top: 16px;
-    }
-
-    /**
-    * Global
-    */
-    .app {
-        height: 100vh;
-    }
-    :global(.container) {        
-        padding: 0 var(--spacing-4);
-    }
-
-    .container.outer {
-        padding: 0;
-    }
-
-    .container.nav {
-        overflow-y: auto;
-        padding-top: 72px;
-        height: 100%;
-    }
-
-    .mobile .container.nav,
-    .tablet .container.nav {
-        padding-left: 24px;
-        padding-right: 24px;
-    }
 
     main {
         min-height: 400px;
         padding-top: 72px;
     }
-    aside {
-        position: fixed;
-        width: inherit;
-        background: #fdf6fe;
-        height: 100vh;
-    }
-
-    aside,
-    .sidenav-activator {
-        transition: all 0.4s;
-    }
-
-    .sidenav-activator {
-        position: absolute;
-        z-index: 1;
-        padding: 4px 8px;
-        right: 20px;
-        top: 16px;
-        font-weight: bolder;
-        background: white;
-        border-radius: 4px;
-        cursor: pointer;
-        border: 2px #ffa8fa solid;
-    }
-
-    :global(.closed) .sidenav-activator {
-        right: -48px;
-    }
 
     /**
     * Mobile
     */
-    .mobile main {
+    :global(.mobile) main {
         padding: var(--spacing-6) var(--spacing-4);
     }
-    :global(.mobile .container) {
-        padding: 0;
-    }
-
-    .mobile aside,
-    .tablet aside {
-        top: 0;
-        height: 100%;
-        z-index: 1;
-        background: white;
-    }
-
-    .mobile.open aside {
-        box-shadow: 0 0 18px -3px rgba(0 0 0 / 0.4);
-    }
-
-    /**
-    * Mobile
-    */
-    .mobile main {
-        margin: 0;
-        border-radius: 0;
-    }
-
-    /**
-    * Desktop
-    */
-
-    .desktop .links,
-    .desktop .sidenav-activator {
-        display: none;
-    }
-
 </style>
