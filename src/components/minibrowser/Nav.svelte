ea09ea2b108be75f36bd999e4e130629ced30d5c
Jakob Rosenberg <jakobrosenberg@gmail.com>
2024-03-25 15:58:35 +0100

    fix: minibrowser nav css

--- src/components/minibrowser/Nav.svelte
+++ src/components/minibrowser/Nav.svelte
@@ -1,55 +1,28 @@
 <script>
+    import Svg from './Svgs.svelte'
+    /** @type { Router['activeRoute'] }*/
     export let activeRoute
-    $: route = $activeRoute
-    $: router = route?.router || {}
-    $: ({ nextRoute, prevRoute } = router || {})
-    $: console.log({ nextRoute, prevRoute })
+
     $: goHome = () => $activeRoute.router.url.push('/')
     $: goBack = () => $activeRoute.router.goBack()
     $: goForward = () => $activeRoute.router.goForward()
 </script>
 
 <div class="nav">
-    {#if $prevRoute}
-        <div on:click={goBack}>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="black"
-                width="18px"
-                height="18px">
-                <path d="M0 0h24v24H0z" fill="none" />
-                <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
-            </svg>
-        </div>
+    {#if $activeRoute?.prevRoute}
+        <button on:click={goBack}>
+            <Svg icon="back" />
+        </button>
     {/if}
-    {#if $nextRoute}
-        <div on:click={goForward}>
-            <!-- forward arrow svg -->
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="black"
-                width="18px"
-                height="18px">
-                <path d="M0 0h24v24H0z" fill="none" />
-                <path d="M4 13h12.17l-5.59 5.59L12 20l8-8-8-8-1.41 1.41L16.17 11H4z" />
-            </svg>
-        </div>
+    {#if $activeRoute?.nextRoute}
+        <button on:click={goForward}>
+            <Svg icon="forward" />
+        </button>
     {/if}
 
-    <div on:click={goHome}>
-        <!-- home svg -->
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="black"
-            width="18px"
-            height="18px">
-            <path d="M0 0h24v24H0z" fill="none" />
-            <path d="M12 2L2 12h3v8h6v-6h4v6h6v-8h3L12 2z" />
-        </svg>
-    </div>
+    <button on:click={goHome}>
+        <Svg icon="home" />
+    </button>
 
     <!-- back arrow svg -->
 </div>
@@ -58,11 +31,16 @@
     .nav {
         display: flex;
         gap: 1rem;
-        
     }
-    .nav > div {
+    .nav button {
         cursor: pointer;
-        display: flex;
         user-select: none;
+        background: none;
+        border: none;
+        cursor: pointer;
+        padding: 0;
+        margin: 0;
+        display: flex;
+        height: auto;
     }
 </style>
---
+++ src/components/minibrowser/Svgs.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let fill = '#555'
+    export let secondaryFill = 'none'
+    export let width = '18px'
+    export let height = '18px'
+    export let icon = 'home'
+    export let viewBox = '0 0 24 24'
+    export let xmlns = 'http://www.w3.org/2000/svg'
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" {viewBox} {fill} {width} {height}>
+    {#if icon === 'home'}
+        <path d="M0 0h24v24H0z" fill={secondaryFill} />
+        <path d="M12 2L2 12h3v8h6v-6h4v6h6v-8h3L12 2z" />
+    {:else if icon === 'forward'}
+        <path d="M0 0h24v24H0z" fill={secondaryFill} />
+        <path d="M4 13h12.17l-5.59 5.59L12 20l8-8-8-8-1.41 1.41L16.17 11H4z" />
+    {:else if icon === 'back'}
+        <path d="M0 0h24v24H0z" fill={secondaryFill} />
+        <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
+    {/if}
+</svg>
