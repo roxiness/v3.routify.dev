ad4cfb9113e81fd1742e576f019147664fd2438e
Jakob Rosenberg <jakobrosenberg@gmail.com>
2025-02-12 12:01:39 +0100

    fix: lazy load features/youtube section

---
+++ src/routes/_features/__components/ShowOnIntersection.svelte
@@ -0,0 +1,14 @@
+<script>
+    let isInView
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) isInView = true
+        })
+    })
+</script>
+
+<div use:observer.observe>
+    {#if isInView}
+        <slot />
+    {/if}
+</div>
--- src/routes/_features/_module.svelte
+++ src/routes/_features/_module.svelte
@@ -1,12 +1,15 @@
 <script>
     import Carousel from '@roxi/routify/components/Carousel.svelte'
     import Nav from './__components/Nav.svelte'
+    import ShowOnIntersection from './__components/ShowOnIntersection.svelte'
     import './features.css'
 </script>
 
 <div class="routify-feature-box">
-    <Carousel class="features">
-        <slot inline />
-    </Carousel>
+    <ShowOnIntersection>
+        <Carousel class="features">
+            <slot inline />
+        </Carousel>
+    </ShowOnIntersection>
     <Nav />
 </div>
